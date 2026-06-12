43b62e0f052ff8099fdb6716eb3a9a4937198662
Luana <luana.almeida@academico.ifpb.edu.br>
2026-06-12 09:39:01 -0300

    adicionar, listar e remover viagem. Terminei a parte de Luana, Alice continua

--- viagem.py
+++ viagem.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 from servidor import Servidor
+from viagemDados import Viagem
+
+viagens = []
+historico_remocoes = []
 
 def carregar_dados():
     dados = {}
@@ -11,8 +15,15 @@ def carregar_dados():
     
     arquivo = open("agencia.txt", "r", encoding="utf-8")
     for linha in arquivo:
-        cpf,nome,tel = linha.strip().split(" : ")
-        dados[cpf] = Servidor(cpf,nome,tel)
+        partes = linha.strip().split(" : ")
+
+        if partes[0] == "S":
+            _, cpf, nome, tel = partes
+            dados[cpf] = Servidor(cpf, nome, tel)
+
+        elif partes[0] == "V":
+            _, cpf, destino, inicio, fim, valor = partes
+            viagens.append(Viagem(cpf, destino, inicio, fim, float(valor)))
        
         
     arquivo.close()
@@ -24,10 +35,13 @@ def salvar_dados(dados):
     for servidor in dados.values():
         arquivo.write(f"{servidor.linha_armazenamento()}\n")
         
+    for viagem in viagens:
+        arquivo.write(f"V : {viagem.linha_armazenamento()}\n")
+
     arquivo.close()
 
 def menu():
-    print("Agencia de viagens")
+    print("\nAgência de viagens")
     print("1. Cadastro de servidores")
     print("2. Cadastro de viagens")
     print("3. Remover viagem")
@@ -35,7 +49,7 @@ def menu():
     print("5. Listar viagens")
     print("6. Sair")
 
-    op = input("Opção: ")
+    op = input("Escolha uma opção: ")
 
     if op.isdigit() and 1 <= int(op) <= 6: 
         return op
@@ -47,12 +61,11 @@ def cadastrar_servidor(dados):
     cpf = input("Digite o CPF (somente números): ").strip()
     nome = input("Digite o nome: ").strip()
     tel = input("Digite o telefone: ").strip()
-    #Luana adiciona as viagens <3(alice mandou)
+
 
     if nome == "" or tel == "" or cpf == "":
-        print("Todos os dados são obrigatórios. Tente novamente")
-        return
-    
+        return "Todos os dados são obrigatórios. Tente novamente"
+        
     if validar_cpf(cpf, dados):
         dados[cpf] = Servidor(cpf, nome, tel)
 
@@ -63,6 +76,60 @@ def cadastrar_servidor(dados):
 def validar_cpf(cpf, dados):
     return cpf not in dados
     
+
+def adicionar_viagem(dados):
+    cpf = input("CPF do servidor: ").strip()
+
+    if cpf not in dados:
+        return "Servidor não encontrado."
+
+    destino = input("Destino: ").strip()
+    inicio = input("Data de início: ").strip()
+    fim = input("Data de fim: ").strip()
+    valor = float(input("Valor das diárias: ").strip())
+
+    viagens.append(Viagem(cpf, destino, inicio, fim, valor))
+
+    return "Viagem cadastrada com sucesso."
+
+
+def remover_viagem():
+    if len(viagens) == 0:
+        return "Nenhuma viagem cadastrada."
+
+    for i, viagem in enumerate(viagens, start=1):
+        print (
+            f"{i} - Destino: {viagem.destino} | "
+            f"Data de início: {viagem.inicio} | "
+            f"Data de fim: {viagem.fim}"
+        )
+
+    indice = int(input("Informe o número da viagem: ")) - 1
+
+    if 0 <= indice < len(viagens):
+        historico_remocoes.append(viagens[indice])
+        viagens.pop(indice)
+        return "Viagem removida com sucesso."
+
+    return "Índice inválido."
+
+
+def listar_viagens():
+    
+    if len(viagens) == 0:
+        return "Nenhuma viagem cadastrada."
+
+    texto = ""
+
+    for indice, viagem in enumerate(viagens, start=1):
+        texto += (
+            f"{indice}. CPF: {viagem.cpf} | "
+            f"Destino: {viagem.destino} | "
+            f"Período: {viagem.inicio} até {viagem.fim} | "
+            f"Valor: {viagem.valor}\n"
+        )
+
+    return texto
 
     
 #Programa principal
@@ -75,13 +142,13 @@ while op != "6":
         case "1":
             print(cadastrar_servidor(dados))
         case "2": 
-            print("viagem")
+            print(adicionar_viagem(dados))
         case "3":
-            print("Remover")
+            print(remover_viagem())
         case "4":
             print("Buscar")
         case "5":
-            print("Listar")
+            print(listar_viagens())
 
     op = menu()
     
