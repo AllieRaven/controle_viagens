e8a804d9ab5ad4b3de111f279ce18be9272fc49a
Alice <130313279+AllieRaven@users.noreply.github.com>
2026-06-15 19:05:22 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -3,9 +3,6 @@ class Servidor:
         self.nome = nome
         self.cpf = cpf
         self.tel = tel
-        
-    def __str__ (self):
-        return f'{self.cpf}: {self.nome}, {self.tel}'
     
     def linha_armazenamento (self):
         return f'S : {self.cpf} : {self.nome} : {self.tel}'
