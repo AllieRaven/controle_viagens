b7e6bed40682667b97b2031d9e99c9cf569056bb
Alice <costa.alice@academico.ifpb.edu.br>
2026-06-15 10:53:53 -0300

    Adição de tabelas e correções menores

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from viagem import Viagem
 from rich.panel import Panel
 from rich import print
 from rich.prompt import Prompt
+from rich.table import Table
 
 # Lista que armazena todas as viagens cadastradas
 viagens = []
@@ -31,22 +32,21 @@ def carregar_dados():
     if not arquivo.is_file():
         return dados
     
-    arquivo = open("agencia.txt", "r", encoding="utf-8")
-    for linha in arquivo:
-        partes = linha.strip().split(" : ")
+    #Torna o abrir e fechar do arquivo automático, deixando o código mais limpo.
+    with open("agencia.txt", "r", encoding="utf-8") as arquivo:
+        for linha in arquivo:
+            partes = linha.strip().split(" : ")
 
-        # Registro de servidor
-        if partes[0] == "S":
-            _, cpf, nome, tel = partes
-            dados[cpf] = Servidor(cpf, nome, tel)
-        
-        # Registro de viagem
-        elif partes[0] == "V":
-            _, cpf, destino, inicio, fim, valor = partes
-            viagens.append(Viagem(cpf, destino, inicio, fim, float(valor)))
-       
-        
-    arquivo.close()
+            #Verifica se é Servidor
+            if partes[0] == "S":
+                _, cpf, nome, tel = partes
+                dados[cpf] = Servidor(cpf, nome, tel)
+
+            #Verifica se é Viagem
+            elif partes[0] == "V":
+                _, cpf, destino, inicio, fim, valor = partes
+                viagens.append(
+                    Viagem(cpf, destino, inicio, fim, float(valor)))
     return dados
 
 
@@ -56,68 +56,64 @@ def salvar_dados(dados):
     no arquivo agencia.txt.
     """
 
-    arquivo = open("agencia.txt", "w", encoding="utf-8")
-    
-    # Salva os servidores
-    for servidor in dados.values():
-        arquivo.write(f"{servidor.linha_armazenamento()}\n")
-    
-    # Salva as viagens
-    for viagem in viagens:
-        arquivo.write(f"{viagem.linha_armazenamento()}\n")
+    with open("agencia.txt", "w", encoding="utf-8") as arquivo:
+        for servidor in dados.values():
+            arquivo.write(f"{servidor.linha_armazenamento()}\n")
 
-    arquivo.close()
+        for viagem in viagens:
+            arquivo.write(f"{viagem.linha_armazenamento()}\n")
 
 
 def menu():
-    """
-    Exibe o menu principal e valida a opção escolhida.
-    """
+    while True:
+        print("\nAgência de viagens")
+        print("[bold][1.] :bust_in_silhouette: Cadastro de servidores[/]")
+        print("[bold][2.] :airplane_departure: Cadastro de viagens[/]")
+        print("[bold][3.] :wastebasket: Remover viagem[/]")
+        print("[bold][4.] :mag: Buscar viagem por servidor[/]")
+        print("[bold][5.] :clipboard: Listar viagens[/]")
+        print("[bold][6.] :door: Sair[/]\n")
 
-    print("\nAgência de viagens")
-    print("[bold][1.] :bust_in_silhouette: Cadastro de servidores[/]")
-    print("[bold][2.] :airplane_departure: Cadastro de viagens[/]")
-    print("[bold][3.] :wastebasket: Remover viagem[/]")
-    print("[bold][4.] :mag: Buscar viagem por servidor[/]")
-    print("[bold][5.] :clipboard: Listar viagens[/]")
-    print("[bold][6.] :door: Sair[/]\n")
+        op = input("Escolha uma opção: ")
 
-    op = Prompt.ask("[bold]Escolha uma opção: [/]")
+        if op.isdigit() and 1 <= int(op) <= 6:
+            return op
 
-    if op.isdigit() and 1 <= int(op) <= 6: 
-        return op
-    else:
-        print("[bold yellow]:warning:Opção inválida. Você deve escolher uma opção entre 1 e 6.[/]")
-        return menu()
+        print("Opção inválida. Você deve escolher uma opção entre 1 e 6.")
 
 
 def cadastrar_servidor(dados):
     print(Panel((":bust_in_silhouette: Cadastro de servidores"), expand=False))
     print()
+
+    #Inicia a função
+
+    #CPF
     while True:
         cpf = Prompt.ask("[bold] :page_facing_up: Digite o CPF (somente números): \n->[/bold]").strip()
 
-        if not cpf:
+        if not cpf: #Se é vazio
             print("[bold yellow]:warning: CPF é obrigatório. Tente novamente[/]")
-        elif not cpf.isdigit() or len(cpf) != 11:
+        elif not cpf.isdigit() or len(cpf) != 11: #Se possui apenas números e a quantidade correta de digitos
             print("[bold yellow]:warning: CPF inválido. Tente novamente[/]")
         else:
             break
 
     nome = Prompt.ask("[bold] :bust_in_silhouette: Digite o nome: \n->[/bold]").strip()
 
-    while not nome:
+    while not nome: #Se é vazio
         print("[bold yellow]:warning: Nome é obrigatório. Tente novamente[/]")
         nome = Prompt.ask("[bold] :bust_in_silhouette: Digite o nome: \n->[/bold]").strip()
 
     while True:
         tel = Prompt.ask("[bold] :telephone_receiver: Digite o telefone(DD NNNNNNNNN): \n ->[/bold]").strip().replace(" ","")
 
-        if not tel.isdigit() or len(tel) != 11:
+        if not tel.isdigit() or len(tel) != 11: #Se possui apenas números e a quantidade correta de digitos
             print("[bold yellow]:warning: Telefone inválido. Tente novamente[/]")
         else:
             break
 
+    #Após todos os dados preenchidos ele checa se o servidor já está cadastrado
     if validar_cpf(cpf, dados):
         dados[cpf] = Servidor(cpf, nome, tel)
         return "[bold green]:white_check_mark: Servidor cadastrado com sucesso![/]"
@@ -130,7 +126,7 @@ def validar_cpf(cpf, dados):
     Verifica se o CPF ainda não está cadastrado.
     Retorna True se estiver disponível.
     """
-
+    #Se não tivereste cpf -> True
     return cpf not in dados
 
 
@@ -140,6 +136,7 @@ def validar_data(data):
     e está no formato dd/mm/aaaa.
     """
 
+    #Valida se a data é valida
     try:
         datetime.strptime(data, "%d/%m/%Y")
         return True
@@ -153,6 +150,7 @@ def periodo_valido(inicio, fim):
     à data inicial da viagem.
     """
 
+    #Valida se a data inicial vem antes da data final
     data_inicio = datetime.strptime(inicio, "%d/%m/%Y")
     data_fim = datetime.strptime(fim, "%d/%m/%Y")
 
@@ -164,12 +162,18 @@ def adicionar_viagem(dados):
     print(Panel((":bust_in_silhouette: Cadastro de viagens"), expand=False))
     print()
 
+    #Chama o cpf para a realização das pesquisas
     cpf = Prompt.ask("[bold]:page_facing_up: Digite o CPF (somente números):\n->[/]").strip()
 
     if cpf not in dados:
         return "[bold red]❌ Servidor não encontrado.[/]"
-
-    destino = Prompt.ask("[bold]:round_pushpin: Destino:\n->[/]").strip()
+    
+    while True:
+        destino = Prompt.ask("[bold]:round_pushpin: Destino:\n->[/]").strip()
+        if(destino == ""):
+            print("[bold red]❌ O destino é obrigatório. [/]")
+        else:
+            break
 
     while True:
         inicio = Prompt.ask("[bold]📅 Data de início (dd/mm/aaaa):\n->[/]").strip()
@@ -197,7 +201,11 @@ def adicionar_viagem(dados):
     return "[bold green]:white_check_mark: Viagem cadastrada com sucesso![/]"
 
 
-def remover_viagem():
+def mascarar_cpf(cpf):
+    return cpf[:3] + ".*.*-" + cpf[-2:]
+
+
+def remover_viagem(dados):
     print(Panel((":bust_in_silhouette: Remoção de viagens"), expand=False))
     print()
 
@@ -213,24 +221,34 @@ def remover_viagem():
 
     viagens_cpf = []
 
+    #Adiciona as viagens relacionadas ao cpf informado em uma lista
     for i, viagem in enumerate(viagens):
         if viagem.cpf == cpf:
             viagens_cpf.append(i)
 
     if len(viagens_cpf) == 0:
         return "[bold yellow]:warning: Este servidor não possui viagens cadastradas.[/]"
+    
+    tabela = Table(title="Viagens do servidor")
+    tabela.add_column("N°")
+    tabela.add_column("Destino")
+    tabela.add_column("Data de início")
+    tabela.add_column("Data de fim")
 
+    
+    #Retorna os dados principais de cada viagem do servidor
     for i, indice in enumerate(viagens_cpf, start=1):
         viagem = viagens[indice]
-
-        print(
-            f"{i} - [bold]:round_pushpin: Destino: {viagem.destino} | [/]"
-            f"[bold]📅 Data de início: {viagem.inicio} | [/]"
-            f"[bold]📅 Data de fim: {viagem.fim}[/]"
-        )
+        tabela.add_row(
+            str(i),
+            viagem.destino,
+            viagem.inicio,
+            viagem.fim)
+        print(tabela)
 
     while True:
         try:
+            #Usuário informa o índice 
             escolha = int(
                 Prompt.ask("[bold]Informe o número da viagem: [/]")
             ) - 1
@@ -249,29 +267,38 @@ def remover_viagem():
         except ValueError:
             print("[bold yellow]:warning: Valor inválido![/]")
 
-def listar_viagens():
-    print(Panel((":bust_in_silhouette: Listar viagens"), expand=False))
-    print()
-    if len(viagens) == 0:
-        return "[bold yellow] :warning: Nenhuma viagem cadastrada.[/]"
 
+def listar_viagens():
+    print(Panel(":bust_in_silhouette: Listar viagens", expand=False))
+    print()
+
+    if len(viagens) == 0:
+        return "[bold yellow]:warning: Nenhuma viagem cadastrada.[/]"
+
+    # Ordena por data
     ordenadas = sorted(
         viagens,
-        key=lambda v: datetime.strptime(v.inicio, "%d/%m/%Y"))
+        key=lambda v: datetime.strptime(v.inicio, "%d/%m/%Y")
+    )
 
-    texto = ""
+    tabela = Table(title="Viagens cadastradas")
+
+    tabela.add_column("CPF")
+    tabela.add_column("Destino")
+    tabela.add_column("Data de início")
+    tabela.add_column("Data de fim")
+    tabela.add_column("Valor das diárias")
 
     for viagem in ordenadas:
-        texto += (
-            f"[bold]:page_facing_up: CPF:[/] {viagem.cpf} | "
-            f"[bold]:round_pushpin: Destino:[/] {viagem.destino} | "
-            f"[bold]📅 Data de início:[/]  {viagem.inicio} | "
-            f"[bold]📅 Data de fim:[/] {viagem.fim} | "
-            f"[bold]💰 Valor das diárias:[/] R$ {viagem.valor:.2f}"
-            f"\n"
+        tabela.add_row(
+            mascarar_cpf(viagem.cpf),
+            viagem.destino,
+            viagem.inicio,
+            viagem.fim,
+            f"R$ {viagem.valor:.2f}"
         )
 
-    return texto
+    return tabela
 
 
 def buscar_viagens_cpf():
@@ -280,27 +307,41 @@ def buscar_viagens_cpf():
     cpf = Prompt.ask("[bold]:page_facing_up: Digite o cpf(apenas números): [/]").strip()
     texto = ""
 
+    #Ordena as viagens por data
     ordenadas = sorted(
         viagens,
         key=lambda v: datetime.strptime(v.inicio, "%d/%m/%Y"))
+    
+    tabela = Table(title=f"Viagens do CPF {cpf}")
+
+    tabela.add_column("Destino")
+    tabela.add_column("Início")
+    tabela.add_column("Fim")
+    tabela.add_column("Valor diária")
+    tabela.add_column("Total")
+
+    encontrou = False
 
     for viagem in ordenadas:
         if viagem.cpf == cpf:
-            texto += (f"[bold]:round_pushpin: Destino: {viagem.destino} | [/]"
-                    f"[bold]📅 Data de início:  {viagem.inicio} | [/]"
-                    f"[bold]📅 Data de fim: {viagem.fim} [/]"
-                    f"[bold]💰 Valor das diárias: R${viagem.valor} [/] |"
-                    f"[bold]💰 Valor total das diárias: R$ {total_diarias(viagem)} [/]"
-                    f"\n"
+            tabela.add_row(
+                viagem.destino,
+                viagem.inicio,
+                viagem.fim,
+                f"R$ {viagem.valor:.2f}",
+                f"R$ {float(total_diarias(viagem)):.2f}"
             )
+            encontrou = True
 
-    if texto:
-        return texto
-    
+    if encontrou:
+        return tabela
+
     return "[bold red]:x: Nenhuma viagem encontrada para este CPF.[/]"
 
 
 def total_diarias(viagem):
+    #Função auxilair. 
+    # As datas já são verificadas no momento do cadastro da viagem.
     inicio = datetime.strptime(viagem.inicio, "%d/%m/%Y")
     fim = datetime.strptime(viagem.fim, "%d/%m/%Y")
 
@@ -336,7 +377,7 @@ while op != "6":
 
         # Remoção de viagem
         case "3":
-            print(remover_viagem())
+            print(remover_viagem(dados))
 
         # Busca por CPF
         case "4":
